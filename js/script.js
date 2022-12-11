7d61c186ab823637f94b84fcd51dd5818c28a15e
tech <tech@kali.kali>
2022-12-11 21:17:36 +0200

    download cv

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 
 
 // javascript for mobile menuicon
-window.addEventListener("load",()=>{
-    document.getElementById("mainmenu").style.display="none";
-});
 
 function mobilemenu(){
     if(document.getElementById("mainmenu").style.display==="block"){
         document.getElementById("mainmenu").style.display="none";
 
-    }else{
+    }
+    else{
         document.getElementById("mainmenu").style.display="block"; 
     }
 }
